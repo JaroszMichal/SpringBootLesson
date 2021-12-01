8060b3370065af2e76fa38b886401a2929426742
JaroszMichal <jkm@jkm.pl>
2021-12-01 14:47:05 +0100

    zrobione

--- src/main/java/com/example/restservice/ConcatStrings.java
+++ src/main/java/com/example/restservice/ConcatStrings.java
@@ -5,6 +5,14 @@ public class ConcatStrings {
     private final String string1;
     private final String string2;
 
+    public String getString1() {
+        return string1;
+    }
+
+    public String getString2() {
+        return string2;
+    }
+
     public ConcatStrings(String string1, String string2) {
         this.string1 = string1;
         this.string2 = string2;
--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -14,4 +14,11 @@ public class GreetingController {
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
+
+    @PostMapping("/greeting")
+    public String concatStrings(@RequestBody ConcatStrings concatStrings){
+        return concatStrings.getConcat();
+    }
+
+
 }
